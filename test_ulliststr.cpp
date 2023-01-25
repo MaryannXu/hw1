79f705e410803dd09a1a0be8ab37c7da20b4789d
Maryann Xu <mxxu@usc.edu>
2023-01-25 12:14:44 -0800

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,34 @@
 
 #include "ulliststr.h"
 
+using namespace std;
+
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+    ULListStr* dat = new ULListStr();
+    dat->push_back("0");
+    dat->push_back("1");
+    dat->push_back("2");
+    dat->push_back("3");
+    dat->push_back("4");
+    dat->push_back("5");
+    dat->push_back("6");
+    dat->push_back("7");
+    dat->push_back("8");
+    dat->push_back("9");
+    dat->push_back(" 10");
+    dat->push_back("11");
+    dat->push_back("12");
+    //dat->push_front("8");
+    //dat->push_back("1");
+    cout << "Value: " << dat->get(0) <<dat->get(1)<< dat->get(2) <<dat->get(3)<< dat->get(4) <<dat->get(5)<< dat->get(6) <<dat->get(7)<< dat->get(8) <<dat->get(9)<< dat->get(10) <<dat->get(11)<<dat->get(12)<<endl;
+    //cout << "Value: " << dat->get(0) << " " << dat->get(1) << endl;
+    //cout << "Value2: " << dat->get(1) << dat->get(1) << dat->get(1) << dat->get(1) << endl;
+    //<< " " << dat->get(2) << endl;
+    // prints: 8 7 9
+    cout << "Size: " << dat->size() << endl;  // prints 3 since there are 3 strings stored
 
+    delete dat;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,7 +1,9 @@
-foo
+
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
+using namespace std;
 
 ULListStr::ULListStr()
 {
@@ -26,6 +28,117 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val){
+  if (empty()){
+    Item* newp = new Item();
+    newp->prev = NULL;
+    newp->next = NULL;
+    head_ = newp;
+    tail_ = newp;
+    tail_->val[0] = val;
+    tail_->last=(tail_->last)+1;
+  }
+  else if (tail_->last >= (ARRSIZE)){
+    Item* newp = new Item();
+    tail_->next = newp;
+    newp->prev = tail_;
+    newp->next = NULL;
+    tail_ = newp;
+    tail_->val[0] = val;
+    tail_->last=(tail_->last)+1;
+  }
+  else if (tail_->last < (ARRSIZE)){
+    tail_->val[(tail_->last)] = val;
+    tail_->last=(tail_->last)+1;
+  }
+  size_++;
+}
+
+void ULListStr::pop_back(){
+  if (empty()==false){
+    if ((tail_->last) == 0){
+      tail_ = tail_->prev;
+      tail_->next = NULL;
+    }
+    else{
+      tail_->val[(tail_->last)-1] = "";
+      tail_->last = (tail_->last)-1;
+    }
+  }
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val){
+  if (empty()){
+    Item* newp = new Item();
+    newp->prev = NULL;
+    newp->next = NULL;
+    head_ = newp;
+    tail_ = newp;
+    head_->val[ARRSIZE-1] = val;
+    head_->first = ARRSIZE-1;
+    head_->last = ARRSIZE;
+  }
+  else if (head_->first == 0){
+    Item* newp = new Item();
+    newp -> next = head_;
+    head_ -> prev = newp;
+    head_ = newp;
+    head_->val[ARRSIZE-1] = val;
+    head_->first = ARRSIZE-1;
+    head_->last = ARRSIZE;
+  }
+  else if (head_->first>0){
+    head_->val[(head_->first)-1] = val;
+    head_->first=(head_->first)-1;
+  }
+  size_++;
+}
+
+void ULListStr::pop_front(){
+  if (empty()==false){
+    if ((head_->first) == (ARRSIZE-1)){
+      head_ = head_->next;
+      head_->prev = NULL;
+    }
+    else{
+      head_->val[(head_->first)] = "";
+      head_->first = (head_->first)+1;
+    }
+  }
+  size_--;
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  Item* curr = head_;
+  int temp;
+  int itemsPassed = 0;
+  int counter = 0;
+  for(size_t i = 0; i < size_; i++){
+    temp = (curr->first) + (loc-itemsPassed);
+    if (i==loc){
+      return &(curr->val[temp]);
+    }
+
+    counter++;
+    if ((curr->first) + counter >= ARRSIZE){
+      curr = curr->next;
+      itemsPassed += counter;
+      counter = 0;
+    }
+  }
+  return NULL;
+  }
+
+std::string const & ULListStr::back() const{
+  return tail_->val[(tail_->last)-1];
+
+}
+
+std::string const & ULListStr::front() const{
+  return head_->val[head_->first];
+
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
